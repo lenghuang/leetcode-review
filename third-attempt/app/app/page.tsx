4e35a3a071b8656d9ad9f0729b20c52d12fd12d8
Len Huang <lendevelops@gmail.com>
2024-11-28 14:37:13 -0500

    start explore / home page

--- third-attempt/app/app/page.tsx
+++ third-attempt/app/app/page.tsx
@@ -11,5 +11,20 @@ export default async function Index() {
     redirect("home");
   }
 
-  return <div>Here will be my app</div>;
+  return (
+    <div>
+      Here will be my app. Here we'll put different tiles for lessons you could
+      explore. Array, trees, graphs, strings, etc. Here, we'll probably need a
+      table for possible topics with our internal representation. As well as a
+      mapping from leetcode topics to our own topics Once user clickes in to
+      something here, we'll be able to go to a new page that has more specific
+      info on that topic.
+      <div className="m-8 flex max-w-96 flex-col gap-8 rounded-xl bg-neutral p-8 text-neutral-content">
+        <div className="flex-1">todo make this a tile component </div>
+        <div className="flex justify-end">
+          <div className="btn btn-circle btn-accent rounded"> Go </div>
+        </div>
+      </div>
+    </div>
+  );
 }
