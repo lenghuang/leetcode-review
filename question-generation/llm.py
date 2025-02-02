70baa3bf332a0c1d47708109e7ab1637862f37a2
Len Huang <lendevelops@gmail.com>
2025-02-01 22:45:41 -0500

    some tweaks

---
+++ question-generation/llm.py
@@ -0,0 +1,46 @@
+from config import Config
+from openai import OpenAI
+
+config = Config()
+
+
+def get_dummy_response():
+
+    try:
+        # OpenAI Client
+        openai_client = OpenAI(api_key=config.OPENAI_API_KEY)
+        completion = openai_client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "developer",
+                    "content": "You are an expert software developer"
+                    + "and give concise advice.",
+                },
+                {"role": "user", "content": "Hello!"},
+            ],
+        )
+        print(completion.choices[0].message)
+    except Exception as e:
+        print(f"An OpenAI error occurred: {e}\n")
+
+    try:
+        # OpenRouter Client
+        open_router_client = OpenAI(
+            api_key=config.OPENROUTER_API_KEY,
+            base_url="https://openrouter.ai/api/v1",
+        )
+        completion = open_router_client.chat.completions.create(
+            model="google/gemini-2.0-flash-thinking-exp:free",
+            messages=[
+                {
+                    "role": "developer",
+                    "content": "You are an expert software developer"
+                    + "and give concise advice.",
+                },
+                {"role": "user", "content": "Hello!"},
+            ],
+        )
+        print(completion.choices[0].message)
+    except Exception as e:
+        print(f"An OpenRouter error occurred: {e}\n")
