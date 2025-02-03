1d4a3d2fcc9c4cb529b03d80b36e5dcc09d67a9e
Len Huang <lendevelops@gmail.com>
2025-02-02 20:02:34 -0500

    note to future len

--- scripts/src/main.py
+++ scripts/src/main.py
@@ -12,6 +12,12 @@ def main():
     # Main execution tasks
     kaggle_generate_grind_75.execute()
 
+    # TODO: Let's look into generating different styles of questions
+    # for the same 75 questions. Such as free text (ask for brute force),
+    # free text (ask for general approach), multiple choice (ask for high
+    # level approach), and finally the existing multiple choice. Once we
+    # have an extensible way to approach that, we can look into extending
+    # it to even more question sets.
 
 if __name__ == "__main__":
     main()
