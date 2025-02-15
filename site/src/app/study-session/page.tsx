4450d6903d26faa8d84c9bcc8fdc678d6aca65ca
Len Huang <lendevelops@gmail.com>
2025-02-14 19:13:06 -0500

    more tweaks

--- site/src/app/study-session/page.tsx
+++ site/src/app/study-session/page.tsx
@@ -3,12 +3,22 @@ import GameArea from '@/components/game-area/GameArea';
 
 export default function StudySession() {
   return (
-    <div className="h-screen w-screen flex md:flex-row flex-col flex-1">
-      <div className="bg-gray-200 w-1/2">
-        <PromptArea />
-      </div>
-      <div className="bg-white w-1/2">
-        <GameArea />
+    <div className="fixed top-12 left-0 mb-16">
+      <div className="flex md:flex-row flex-col flex-1 h-screen w-screen">
+        <div className="flex flex-col bg-gray-200 w-2/5 overflow-y-auto h-screen">
+          <div className="p-4">
+            <PromptArea />
+            <PromptArea />
+            <PromptArea />
+            <PromptArea />
+            This text is at the bottom, can you see it?
+          </div>
+        </div>
+        <div className="flex flex-col justify-center bg-white w-auto h-screen overflow-y-auto">
+          <div className="p-4">
+            <GameArea />
+          </div>
+        </div>
       </div>
     </div>
   );
