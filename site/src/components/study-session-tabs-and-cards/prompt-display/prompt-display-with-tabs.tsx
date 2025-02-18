90a670f39814a3a0cab9cd68b8c425f96f440f95
Len Huang <lendevelops@gmail.com>
2025-02-17 23:32:04 -0500

    improve metadata ui

--- site/src/components/study-session-tabs-and-cards/prompt-display/prompt-display-with-tabs.tsx
+++ site/src/components/study-session-tabs-and-cards/prompt-display/prompt-display-with-tabs.tsx
@@ -1,3 +1,4 @@
+import { FC, ReactNode } from 'react';
 import {
   Card,
   CardContent,
@@ -7,35 +8,58 @@ import {
 } from '@/components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { DescriptionTabDisplayProps } from '@/types/study-session.types';
+import { ExternalLink } from 'lucide-react';
 
 export const PromptDisplayWithTabs = ({ data }: DescriptionTabDisplayProps) => (
-  <Tabs defaultValue={'description'}>
-    <TabsList className="grid w-full grid-cols-2">
-      <TabsTrigger value={'description'}>Description</TabsTrigger>
-      <TabsTrigger value={'solution'}>Solution</TabsTrigger>
-    </TabsList>
-    <TabsContent value={'description'}>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            {data.id}. {data.title}
-          </CardTitle>
-          {/* TODO ADD LEETCODE LINK */}
-          <CardDescription>See the original question here.</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-2">{data.content}</CardContent>
-      </Card>
-    </TabsContent>
-    <TabsContent value={'solution'}>
-      <Card>
-        <CardHeader>
-          <CardTitle>Description</CardTitle>
-          <CardDescription>
-            Read up on the original question before reviewing.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-2">{data.answer}</CardContent>
-      </Card>
-    </TabsContent>
-  </Tabs>
+  <div>
+    <div className="px-1">
+      <h1 className="text-xl font-bold mb-2">
+        {data.id}. {data.title}
+      </h1>
+      <CustomBadgeContainer>
+        <CustomBadge>Difficulty: {data.difficulty}</CustomBadge>
+        <a
+          className="text-sm"
+          target="_blank"
+          href={`https://leetcode.com/problems/${data.slug}`}
+        >
+          <CustomBadge>
+            Original Question <ExternalLink size={'0.875rem'} />
+          </CustomBadge>
+        </a>
+      </CustomBadgeContainer>
+    </div>
+    <Tabs defaultValue={'description'}>
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value={'description'}>Description</TabsTrigger>
+        <TabsTrigger value={'solution'}>Solution</TabsTrigger>
+      </TabsList>
+      <TabsContent value={'description'}>
+        <Card>
+          <CardContent className="space-y-2">{data.content}</CardContent>
+        </Card>
+      </TabsContent>
+      <TabsContent value={'solution'}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Description</CardTitle>
+            <CardDescription>
+              Read up on the original question before reviewing.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">{data.answer}</CardContent>
+        </Card>
+      </TabsContent>
+    </Tabs>
+  </div>
+);
+
+const CustomBadgeContainer: FC<{ children: ReactNode }> = ({ children }) => (
+  <div className="text-sm flex gap-2 mb-4">{children}</div>
+);
+
+const CustomBadge: FC<{ children: ReactNode }> = ({ children }) => (
+  <div className="flex justify-center gap-1 items-center bg-slate-200 rounded px-1">
+    {children}
+  </div>
 );
