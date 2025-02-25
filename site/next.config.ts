aae04c69aa48cffc16b976742dd45849cfd24462
Len Huang <lendevelops@gmail.com>
2025-02-25 08:34:43 -0500

    tweak output directory

--- site/next.config.ts
+++ site/next.config.ts
@@ -5,7 +5,7 @@ const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
   swSrc: 'src/app/sw.ts',
-  swDest: 'src/public/sw.js',
+  swDest: 'public/sw.js',
 });
 
 const nextConfig: NextConfig = {
