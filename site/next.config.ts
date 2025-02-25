067d977cba8e15e2509819e5f91bc1f362adf5b6
Len Huang <lendevelops@gmail.com>
2025-02-25 08:32:22 -0500

    update to reference source folder

--- site/next.config.ts
+++ site/next.config.ts
@@ -4,8 +4,8 @@ import withSerwistInit from '@serwist/next';
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
-  swSrc: 'app/sw.ts',
-  swDest: 'public/sw.js',
+  swSrc: 'src/app/sw.ts',
+  swDest: 'src/public/sw.js',
 });
 
 const nextConfig: NextConfig = {
