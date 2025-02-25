3dc4705d865dcbe9dc288cf2606bb14ebe7ba1b1
Len Huang <lendevelops@gmail.com>
2025-02-25 08:27:41 -0500

    add icons and metadata

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
+import withSerwistInit from '@serwist/next';
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js',
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
