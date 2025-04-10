8e3b1e71217e5e38288fa419ef7e84094c8e6579
Len Huang <lendevelops@gmail.com>
2025-04-09 22:55:40 -0400

    rename

--- extension/config/webpack.config.js
+++ extension/config/webpack.config.js
@@ -10,7 +10,7 @@ const config = (env, argv) =>
   merge(common, {
     entry: {
       popup: PATHS.src + '/popup.ts',
-      contentScript: PATHS.src + '/contentScript.ts',
+      contentScriptSync: PATHS.src + '/contentScriptSync.ts',
       background: PATHS.src + '/background.ts',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
