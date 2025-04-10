c6ebc45126f26c4b36cc69abf00f96e707b5a7dd
Len Huang <32604574+lenghuang@users.noreply.github.com>
2025-04-09 23:11:56 -0400

    chore: Starting Extension from Scratch, Updating Archive (#38)
    ## Description
    
    As a follow up to https://github.com/lenghuang/leetcode-review/pull/36,
    setting up the repo from scratch and starting again
    
    Note, I had to bump typescript version:
    https://github.com/dutiyesh/chrome-extension-cli/issues/58

---
+++ archive/extension-1/README.md
@@ -0,0 +1,10 @@
+# Chrome Extension
+
+Inspirations and references:
+
+- https://developer.chrome.com/docs/extensions/get-started/tutorial/scripts-on-every-tab
+- https://github.com/TheShubham99/leetcode-to-pdf?tab=readme-ov-file
+- https://github.com/vsavko/Leetcode-parser
+- https://github.com/jungsu-kwon/Leetcode-Downloader-for-Submissions
+
+This project was bootstrapped with [Chrome Extension CLI](https://github.com/dutiyesh/chrome-extension-cli)
--- extension/config/webpack.config.js
+++ extension/config/webpack.config.js
@@ -4,26 +4,16 @@ const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 const PATHS = require('./paths');
-const Dotenv = require('dotenv-webpack');
 
 // Merge webpack configuration files
 const config = (env, argv) =>
   merge(common, {
     entry: {
-      // If you want background or ContentScripts, add more entries
-      popup: PATHS.src + '/popup.js',
-      background: PATHS.src + '/background.js',
-      contentScriptLeetcode: PATHS.src + '/contentScriptLeetcode.js',
-      contentScriptSync: PATHS.src + '/contentScriptSync.js',
-      enum: PATHS.src + '/enum.js',
-      utils: PATHS.src + '/utils.js',
+      popup: PATHS.src + '/popup.ts',
+      contentScriptSync: PATHS.src + '/contentScriptSync.ts',
+      background: PATHS.src + '/background.ts',
     },
     devtool: argv.mode === 'production' ? false : 'source-map',
-    plugins: [
-      new Dotenv({
-        path: argv.mode === 'production' ? './.env.production' : './.env.local',
-      }),
-    ],
   });
 
 module.exports = config;
